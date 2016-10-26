9224f730d710e043bfa83e901566fabc3400ce74
rajusci <raju.s@customerinspire.com>
2016-10-26 15:44:59 +0530

    completed flash light

--- app/src/main/java/com/example/raju/androidpractice/flashlight/FlashLightActivity.java
+++ app/src/main/java/com/example/raju/androidpractice/flashlight/FlashLightActivity.java
@@ -2,46 +2,80 @@ package com.example.raju.androidpractice.flashlight;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
 import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 
 import com.example.raju.androidpractice.R;
 
+import java.security.Policy;
+
+/**
+ * Created by Raju on 10/20/2016.
+ */
+
 public class FlashLightActivity extends Activity {
 
-    ImageButton btnSwitch;
+    private Context context;
 
-    private Camera camera;
-    private boolean isFlashOn;
+    // declare flash light image button
+    private ImageButton imgFlashLight;
+
     private boolean hasFlash;
-    Parameters params;
-    MediaPlayer mp;
+    private Camera camera;
+    private Parameters params;
+    private boolean isFlashOn;
+    private MediaPlayer mp;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flash_light);
 
-        // flash switch button
-        btnSwitch = (ImageButton) findViewById(R.id.imgFlashLight);
+        context = FlashLightActivity.this;
 
+        initialize();
+    }
 
-        // First check if device is supporting flashlight or not
-        hasFlash = getApplicationContext().getPackageManager()
-                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    // Method to initialize ui components
+    protected void initialize(){
 
-        if (!hasFlash) {
+        // initialize flash light image button
+        imgFlashLight = (ImageButton)findViewById(R.id.imgFlashLight);
+
+        imgFlashLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isFlashOn){
+
+                    // turn off flash
+                    turnOffFlash();
+
+                }else {
+
+                    // turn on flash
+                    turnOnFlash();
+                }
+            }
+        });
+
+        // First check if device is supporting flash light or not
+        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(getPackageManager().FEATURE_CAMERA_FLASH);
+
+        if(!hasFlash){
+
             // device doesn't support flash
             // Show alert message and close the application
-            AlertDialog alert = new AlertDialog.Builder(FlashLightActivity.this)
+            AlertDialog alert = new AlertDialog.Builder(context)
                     .create();
             alert.setTitle("Error");
             alert.setMessage("Sorry, your device doesn't support flash light!");
@@ -53,6 +87,7 @@ public class FlashLightActivity extends Activity {
             });
             alert.show();
             return;
+
         }
 
         // get the camera
@@ -61,43 +96,39 @@ public class FlashLightActivity extends Activity {
         // displaying button image
         toggleButtonImage();
 
-
-        // Switch button click event to toggle flash on/off
-        btnSwitch.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (isFlashOn) {
-                    // turn off flash
-                    turnOffFlash();
-                } else {
-                    // turn on flash
-                    turnOnFlash();
-                }
-            }
-        });
     }
 
+    /**
+     * Method to get camera
+     */
+    public void getCamera(){
 
-    // Get the camera
-    private void getCamera() {
-        if (camera == null) {
-            try {
+        if(camera == null){
+
+            try{
+
                 camera = Camera.open();
                 params = camera.getParameters();
-            } catch (RuntimeException e) {
+
+            }catch (RuntimeException e){
+
                 Log.e(getString(R.string.err_camera_open), e.getMessage());
             }
         }
     }
 
+    /**
+     * Method to turning on flash
+     */
+    public void turnOnFlash(){
 
-    // Turning On flash
-    private void turnOnFlash() {
-        if (!isFlashOn) {
-            if (camera == null || params == null) {
+        if(!isFlashOn){
+
+            if(camera == null || params == null){
+
                 return;
             }
+
             // play sound
             //playSound();
 
@@ -110,60 +141,77 @@ public class FlashLightActivity extends Activity {
             // changing button/switch image
             toggleButtonImage();
         }
-
     }
 
+    /**
+     * Method to turning off flash
+     */
+    public void turnOffFlash(){
 
-    // Turning Off flash
-    private void turnOffFlash() {
-        if (isFlashOn) {
-            if (camera == null || params == null) {
+        if(isFlashOn){
+
+            if(camera == null || params == null){
+
                 return;
             }
+
             // play sound
             //playSound();
 
             params = camera.getParameters();
             params.setFlashMode(Parameters.FLASH_MODE_OFF);
             camera.setParameters(params);
-            camera.stopPreview();
+            camera.startPreview();
             isFlashOn = false;
 
             // changing button/switch image
             toggleButtonImage();
         }
+
     }
 
+    /**
+     * Playing sound
+     * will play button toggle sound on flash on / off
+     */
+    public void playSound(){
 
-    // Playing sound
-    // will play button toggle sound on flash on / off
-    private void playSound(){
         if(isFlashOn){
-            mp = MediaPlayer.create(FlashLightActivity.this, R.raw.light_switch_off);
-        }else{
-            mp = MediaPlayer.create(FlashLightActivity.this, R.raw.light_switch_on);
-        }
-        mp.setOnCompletionListener(new OnCompletionListener() {
 
+            mp = MediaPlayer.create(context, R.raw.light_switch_off);
+
+        }else {
+
+            mp = MediaPlayer.create(context, R.raw.light_switch_on);
+        }
+
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
+
                 // TODO Auto-generated method stub
                 mp.release();
             }
         });
         mp.start();
+
     }
 
     /*
-     * Toggle switch button images
-     * changing image states to on / off
-     * */
-    private void toggleButtonImage(){
+    * Toggle switch button images
+    * changing image states to on / off
+    * */
+    public void toggleButtonImage(){
+
         if(isFlashOn){
-            btnSwitch.setImageResource(R.drawable.flash_on);
-        }else{
-            btnSwitch.setImageResource(R.drawable.flash_off);
+
+            imgFlashLight.setImageResource(R.drawable.flash_on);
+
+        }else {
+
+            imgFlashLight.setImageResource(R.drawable.flash_off);
         }
+
     }
 
     @Override
@@ -211,5 +259,4 @@ public class FlashLightActivity extends Activity {
             camera = null;
         }
     }
-
 }
