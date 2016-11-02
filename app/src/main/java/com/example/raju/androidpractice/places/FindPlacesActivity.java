87a3861d15f9ccb32d8eb961d2fb21c2307d1151
rajusci <raju.s@customerinspire.com>
2016-11-02 16:44:10 +0530

    doing find places operation

--- app/src/main/java/com/example/raju/androidpractice/places/FindPlacesActivity.java
+++ app/src/main/java/com/example/raju/androidpractice/places/FindPlacesActivity.java
@@ -3,7 +3,13 @@ package com.example.raju.androidpractice.places;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import com.ui.controller.sliding.DrawerLayoutListener;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.example.raju.androidpractice.R;
@@ -17,7 +23,11 @@ public class FindPlacesActivity extends Activity {
 
     private Context context;
 
+    private FloatingActionButton fabAddPlaces;
+
     private LinearLayout layFindPlaceContainer;
+
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,8 @@ public class FindPlacesActivity extends Activity {
         setContentView(R.layout.activity_find_places);
 
         context = FindPlacesActivity.this;
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
 
         initialize();
     }
@@ -34,10 +46,39 @@ public class FindPlacesActivity extends Activity {
      */
     public void initialize(){
 
+        // initialize Linear layout
         layFindPlaceContainer = (LinearLayout)findViewById(R.id.layFindPlaceContainer);
 
-        Snackbar.make(layFindPlaceContainer, "Replace with your own action", Snackbar.LENGTH_LONG)
+        Snackbar.make(layFindPlaceContainer, "Replace with your own action", Snackbar.LENGTH_INDEFINITE)
                 .setAction("Action", null).show();
+
+        // initialize floating action button
+        fabAddPlaces = (FloatingActionButton)findViewById(R.id.fabAddPlaces);
+
+        fabAddPlaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openNavigationDrawer();
+
+                /*DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+                //drawerLayout.setDrawerListener(new DrawerLayoutListener(R.id.nav_view));
+
+                drawerLayout.openDrawer(Gravity.RIGHT);*/
+            }
+        });
+
+    }
+
+    /**
+     * Method to open the naviagtion drawer
+     * when the fab button clicked
+     */
+    public void openNavigationDrawer(){
+
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawer.openDrawer(Gravity.LEFT);
 
     }
 }
